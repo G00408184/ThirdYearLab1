ce52308fbd44a24b6520ee6084a48761e561e73c
Jakub Mamala <g00408184@atu.ie>
2023-10-09 20:52:59 +0100

    Forgot to push my pom file so made a new project and pushed

---
+++ src/main/java/ie/atu/thirdyearlab1/ThirdYearLab1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.thirdyearlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdYearLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdYearLab1Application.class, args);
+    }
+
+}
